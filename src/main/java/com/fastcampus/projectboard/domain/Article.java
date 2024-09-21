2dfebee53042bdc467d4c24b86b5381be3c499c7
jina <jinahoi@naver.com>
2024-09-21 11:14:24 +0900

    #3 - 게시판의 게시글, 댓글 도메인 설계
     게시판 서비스에 사용할 데이터 2가지를 설계했습니다.
    
    - hashtag: 기획팀에서 해시태그 검색 기능 요구
    - Comment: Article을 추가해서 어떤 게시글의 댓글인지 알 수 있도록 구현

---
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -0,0 +1,15 @@
+package com.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private String title;
+    private String content;
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
