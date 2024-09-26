7143a5e3b50b7bd06fa1c5db47e96e586b26f351
jina <jinahoi@naver.com>
2024-09-27 01:40:28 +0900

    #7 - 도메인 코드를 엔티티로 변환
    * 각 클래스가 하나의 테이블이 되도록 설계
    * Equals(), HashCode() 를 도메인 맥락에 맞도록 재정의

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,15 +1,73 @@
 package com.fastcampus.projectboard.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
+@Getter
+@ToString
+@Table(indexes = { // 특정 필드에 인덱스 추가로 검색 속도 향상
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
-    private String title;
-    private String content;
-    private String hashtag;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 테이블의 PK로 사용할 id 필수
 
+    // setter 를 각 필드에 지정해서 유연하게 변경 가능
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false, length = 10000) private String content; // 내용 최대 10000자로 제한
+    @Setter private String hashtag;
+
+    @ToString.Exclude // ToString 에서 제외할 필드
+    @OrderBy("id") // ArticleComment 를 id 기준으로 정렬
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // orphanRemoval = false 상태
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 중복 불허 (equals(), hashCode()로 판단), 순서 보장
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자 이름 수 제한
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자 이름 수 제한
+
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    // 외부에서 아래 정적 팩토리 메소드(of)로 객체 생성 유도
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;  // 현재 객체와 비교 객체가 동일하면 true
+        if (!(o instanceof Article that)) return false; // 비교 객체 타입이 Article 이 아니면 false
+        return id != null && id.equals(that.id); // id 가 null 이 아니고 두 객체 id가 같으면 true
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
