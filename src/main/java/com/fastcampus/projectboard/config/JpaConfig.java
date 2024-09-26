bfed81248f2e7af728415b71d8c452c050d3fab7
jina <jinahoi@naver.com>
2024-09-27 01:36:42 +0900

    #7 - Spring Data JPA 속성 및 설정
    * JpaConfig: Jpa Auditing 사용을 위해 설정 + 수정 필요
    * application.yaml: DB와 JPA 관련 설정 추가

---
+++ src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.fastcampus.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("authenticated user"); //TODO: Spring Security 인증 기능 추가되면 수정 필요
+    }
+}
