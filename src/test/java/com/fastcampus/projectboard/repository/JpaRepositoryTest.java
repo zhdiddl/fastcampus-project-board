dceb182bd6c8e3f2c4603a5519dab9e43ddbd71d
jina <jinahoi@naver.com>
2024-09-28 16:15:31 +0900

    #7 - Spring Data JPA 테스트 수정
    인메모리 DB와 실제 DB에서 모두 JPA 테스트가 가능하도록 수정

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -1,11 +1,12 @@
 package com.fastcampus.projectboard.repository;
 
-
 import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
@@ -13,79 +14,110 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("JPA 연결 테스트")
-@Import(JpaConfig.class)
-@DataJpaTest
-public class JpaRepositoryTest {
+@DisplayName("[DB] JPA 연결 테스트")
+class JpaRepositoryTest {
 
-    private final ArticleRepository articleRepository;
-    private final ArticleCommentRepository articleCommentRepository;
+    @Nested
+    @DisplayName("인-메모리 DB 테스트")
+    class InMemoryDBTest extends DBTest {
 
-    public JpaRepositoryTest(
-            @Autowired ArticleRepository articleRepository,
-            @Autowired ArticleCommentRepository articleCommentRepository
-    ) {
-        this.articleRepository = articleRepository;
-        this.articleCommentRepository = articleCommentRepository;
+        public InMemoryDBTest(
+                @Autowired ArticleRepository articleRepository,
+                @Autowired ArticleCommentRepository articleCommentRepository
+        ) {
+            super(articleRepository, articleCommentRepository);
+        }
+
     }
 
-    @DisplayName("select 테스트")
-    @Test
-    void givenTestData_whenSelecting_thenWorksFine() {
-        // Given
+    @Nested
+    @DisplayName("실제 DB 테스트")
+    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+    class ActualDBTest extends DBTest {
 
-        // When
-        List<Article> articles = articleRepository.findAll();
+        public ActualDBTest(
+                @Autowired ArticleRepository articleRepository,
+                @Autowired ArticleCommentRepository articleCommentRepository
+        ) {
+            super(articleRepository, articleCommentRepository);
+        }
 
-        // Then
-        assertThat(articles)
-                .isNotNull()
-                .hasSize(123);
     }
 
-    @DisplayName("insert 테스트")
-    @Test
-    void givenTestData_whenInserting_thenWorksFine() {
-        // Given
-        long previousCount = articleRepository.count();
 
-        // When
-        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+    @Import(JpaConfig.class)
+    @DataJpaTest
+    private abstract class DBTest {
 
-        // Then
-        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
-    }
+        private final ArticleRepository articleRepository;
+        private final ArticleCommentRepository articleCommentRepository;
 
-    @DisplayName("update 테스트")
-    @Test
-    void givenTestData_whenUpdating_thenWorksFine() {
-        // Given
-        Article article = articleRepository.findById(1L).orElseThrow();
-        String updatedHashtag = "#springboot";
-        article.setHashtag(updatedHashtag);
+        public DBTest(
+                ArticleRepository articleRepository,
+                ArticleCommentRepository articleCommentRepository
+        ) {
+            this.articleRepository = articleRepository;
+            this.articleCommentRepository = articleCommentRepository;
+        }
 
-        // When
-        Article savedArticle = articleRepository.saveAndFlush(article);
+        @DisplayName("select 테스트")
+        @Test
+        void givenTestData_whenSelecting_thenWorksFine() {
+            // Given
 
-        // Then
-        assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
-    }
+            // When
+            List<Article> articles = articleRepository.findAll();
 
-    @DisplayName("delete 테스트")
-    @Test
-    void givenTestData_whenDeleting_thenWorksFine() {
-        // Given
-        Article article = articleRepository.findById(1L).orElseThrow();
-        long previousArticleCount = articleRepository.count();
-        long previousArticleCommentCount = articleCommentRepository.count();
-        int deletedCommentsSize = article.getArticleComments().size();
+            // Then
+            assertThat(articles)
+                    .isNotNull()
+                    .hasSize(123);
+        }
 
-        // When
-        articleRepository.delete(article);
+        @DisplayName("insert 테스트")
+        @Test
+        void givenTestData_whenInserting_thenWorksFine() {
+            // Given
+            long previousCount = articleRepository.count();
 
-        // Then
-        assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
-        assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+            // When
+            articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+            // Then
+            assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+        }
+
+        @DisplayName("update 테스트")
+        @Test
+        void givenTestData_whenUpdating_thenWorksFine() {
+            // Given
+            Article article = articleRepository.findById(1L).orElseThrow();
+            String updatedHashtag = "#springboot";
+            article.setHashtag(updatedHashtag);
+
+            // When
+            Article savedArticle = articleRepository.saveAndFlush(article);
+
+            // Then
+            assertThat(savedArticle).hasFieldOrPropertyWithValue("hashtag", updatedHashtag);
+        }
+
+        @DisplayName("delete 테스트")
+        @Test
+        void givenTestData_whenDeleting_thenWorksFine() {
+            // Given
+            Article article = articleRepository.findById(1L).orElseThrow();
+            long previousArticleCount = articleRepository.count();
+            long previousArticleCommentCount = articleCommentRepository.count();
+            int deletedCommentsSize = article.getArticleComments().size();
+
+            // When
+            articleRepository.delete(article);
+
+            // Then
+            assertThat(articleRepository.count()).isEqualTo(previousArticleCount - 1);
+            assertThat(articleCommentRepository.count()).isEqualTo(previousArticleCommentCount - deletedCommentsSize);
+        }
     }
 
 }
