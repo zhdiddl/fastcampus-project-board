df4e5939c1c1001e5c9158e912a1dfbf7ff06360
jina <jinahoi@naver.com>
2024-10-03 21:40:55 +0900

    #23 - 도메인 수정 - 회원 계정과 게시글, 댓글 관계 연결

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = { // 특정 필드에 인덱스 추가로 검색 속도 향상
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -24,28 +24,31 @@ public class Article extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 테이블의 PK로 사용할 id 필수
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
+
     // setter 를 각 필드에 지정해서 유연하게 변경 가능
     @Setter @Column(nullable = false) private String title;
     @Setter @Column(nullable = false, length = 10000) private String content; // 내용 최대 10000자로 제한
     @Setter private String hashtag;
 
     @ToString.Exclude // ToString 에서 제외할 필드
-    @OrderBy("id") // ArticleComment 를 id 기준으로 정렬
+    @OrderBy("createdAt DESC") // ArticleComment 를 생성일시 내림차순으로 정렬
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // orphanRemoval = false 상태
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>(); // 중복 불허 (equals(), hashCode()로 판단), 순서 보장
 
 
     protected Article() {}
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
     // 외부에서 아래 정적 팩토리 메소드(of)로 객체 생성 유도
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override
