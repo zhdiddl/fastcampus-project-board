9017be1e3ede34c48e2b4c6d98f7b921600bd770
jina <jinahoi@naver.com>
2024-10-03 16:15:18 +0900

    #15 - AuditingFields 추출
    - 생성일시, 생성자, 수정일시, 수정자는 엔티티 클래스에 반복적으로 포함되는 요소이며, 도메인과 직접적으로 관련이 없으므로 별도로 추출한다.
    - @MappedSuperClass 사용해서 상속받는 엔티티 클래스에 정보를 전달한다.

---
+++ src/main/java/com/fastcampus/projectboard/domain/AuditingFields.java
@@ -0,0 +1,43 @@
+package com.fastcampus.projectboard.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass // 상속받는 엔티티 클래스에 데이터를 전달
+public class AuditingFields {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) // ISO 형식으로 동일하게 설정
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 생성일시
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String modifiedBy; // 수정자
+
+}
