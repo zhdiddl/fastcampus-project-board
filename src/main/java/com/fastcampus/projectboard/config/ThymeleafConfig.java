eefa84b61aa4f972b7dfb63e015b0f3c216ebaa6
jina <jinahoi@naver.com>
2024-10-03 19:57:30 +0900

    #21 - Thymeleaf 3 decoupled logic 설정
    Thymeleaf 3의 디커플드 로직(html 파일과 템플릿 로직을 분리하는 기능)을 사용할지 application.properties 파일에서 설정할 수 있게 만든다
    
    *https://gist.github.com/djkeh/6e1d557ce8c466135b1541d342b1c25c

---
+++ src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
@@ -0,0 +1,36 @@
+package com.fastcampus.projectboard.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(
+            SpringResourceTemplateResolver defaultTemplateResolver, // 기본 템플릿 resolver
+            Thymeleaf3Properties thymeleaf3Properties // 사용자 정의 프로퍼티 클래스
+    ) {
+        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic()); // 사용자 정의에 따라 로직 사용 여부 결정
+
+        return defaultTemplateResolver; // 설정 완료된 템플릿 resolver 반환
+    }
+
+
+    @RequiredArgsConstructor
+    @Getter
+    @ConstructorBinding
+    @ConfigurationProperties("spring.thymeleaf3")
+    public static class Thymeleaf3Properties {
+        /**
+         * Use Thymeleaf 3 Decoupled Logic
+         */
+        private final boolean decoupledLogic;
+    }
+
+}
