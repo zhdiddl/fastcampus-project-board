75982498bcc3d83a132fc1b35a5cdbb56bf4b0d7
jina <jinahoi@naver.com>
2024-10-03 16:52:02 +0900

    #17 - REST API 테스트 작성
    - SpringBootTest로 통합 테스트 진행
    - 테스트 완료 후 데이터가 롤백되어 DB에 영향 없도록 설정
    - MockMvc를 설정하고 주입해서 http 요청과 응답을 포함해 테스트

---
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -0,0 +1,83 @@
+package com.fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data REST - API 테스트")
+@Transactional // 테스트 완료 후 데이터가 롤백되어 DB에 영향 없도록 설정
+@AutoConfigureMockMvc // MockMvc 를 스프링 컨텍스트에 설정
+@SpringBootTest // 모든 스프링 빈을 로드해서 통합 테스트
+public class DataRestTest {
+
+    private final MockMvc mvc; // HTTP 요청을 보내고 서버에서 오는 응답 코드와 내용을 가상으로 검증하는 객체
+
+    public DataRestTest(@Autowired MockMvc mvc) { // 객체 생성 시점에 의존성이 주입되어 불변성 보장 (생성자 주입)
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticles_thenReturnArticlesJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles")) // Data REST 에서 애노테이션이 붙은 엔티티 이름으로 경로 생성 (소문자, 복수형으로 변환)
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json"))); // 서버가 반환하는 콘텐츠 타입이 hal 포맷의 json 데이터
+    }
+
+    @DisplayName("[api] 게시글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticle_thenReturnsArticleJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 게시글 -> 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleCommentsFromArticle_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComments_thenReturnsArticleCommentsJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+
+    @DisplayName("[api] 댓글 단건 조회")
+    @Test
+    void givenNothing_whenRequestingArticleComment_thenReturnsArticleCommentJsonResponse() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
